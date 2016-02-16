2a95ede0402da320da74a87c2ddfabb14c283908
Mark Morrison <mjmorrison@Marks-MacBook-Pro-4.local>
2016-02-15 20:28:35 -0500

    First Commit

---
+++ lab4_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title> Dog Club </title>
+        <link rel="stylesheet" type="text/css" href="lab4.css">
+    </head>
+    <body>
+        <table class="main">
+            <p class="mainText">
+                    Welcome to the official website for dog club. Dog club is the best club. It is far better than that cat club. Please fill out the poll to the left of this page concerning when you would be able to meet. 
+            </p>
+            </td>
+    </body>
+</html>
